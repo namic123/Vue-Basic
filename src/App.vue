2b28ad8a660b9c328408be6f8284887c5fbeea93
jay sung <namic123@naver.com>
2024-02-20 17:12:36 +0900

    vue 컴포넌트 사용법. - setup 함수 작성 - html에서 js 변수 사용 - style 적용법

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,27 @@
+<!--Vue 컴포넌트는 크게 3가지 요소로 구성 -->
+<!--1. template이 있고, 이 안에는 html 코드가 구성-->
+<!--2. script, js 코드가 들어간다. -->
+<!--3. style, css 코드가 들어간다 -->
 <template>
-  <div>My first vue app!!</div>
+<!-- 아래 script 코드의 변수를 호출 -->
+  <div class="name">{{name}}</div> 
 </template>
+<script>
+/* Vue 3 : Composition API */
+/* 이 API에서는 관련된 기능을 함께 묶어 "컴포지션 함수"로 만들고 이를 여러 컴포넌트에서 재사용 가능*/
+/* setup 함수는 컴포지션 API의 진입점 역할, 컴포넌트의 설정을 담당하는 함수 */
+export default {
+  setup(){
+    const name = 'jay park';
+
+    return {
+      name
+    };
+  }
+}
+</script>
+<style>
+  .name{
+    color: red;
+  }
+</style>
