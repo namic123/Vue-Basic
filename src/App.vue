a3e5af57423d07727cae634909a31d6a5bd54674
jay sung <namic123@naver.com>
2024-02-20 17:34:16 +0900

    prettier 설정

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,28 @@
 <!--2. script, js 코드가 들어간다. -->
 <!--3. style, css 코드가 들어간다 -->
 <template>
-<!-- 아래 script 코드의 변수를 호출 -->
-  <div class="name">{{name}}</div> 
+  <!-- 아래 script 코드의 변수를 호출 -->
+  <div>
+    <div class="name">{{ name }}</div>
+    <div>Hi</div>
+  </div>
 </template>
 <script>
 /* Vue 3 : Composition API */
 /* 이 API에서는 관련된 기능을 함께 묶어 "컴포지션 함수"로 만들고 이를 여러 컴포넌트에서 재사용 가능*/
 /* setup 함수는 컴포지션 API의 진입점 역할, 컴포넌트의 설정을 담당하는 함수 */
 export default {
-  setup(){
-    const name = 'jay park';
+  setup() {
+    const name = "jay park";
 
     return {
-      name
+      name,
     };
-  }
-}
+  },
+};
 </script>
 <style>
-  .name{
-    color: red;
-  }
+.name {
+  color: red;
+}
 </style>
