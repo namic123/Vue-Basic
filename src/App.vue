c516b44ce85f4c5e0232590b7146c34471641081
jay sung <namic123@naver.com>
2024-02-21 19:16:21 +0900

    v-show 활용 : todo에 빈문자 입력시 에러메세지 toast

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-<!-- 조건문 작성 v-show vs v-if  -->
-<!-- 공식문서 기준 v-if는 toggle시 비용이 많이 발생하고 v-show는 초기 렌더링에 비용이 많이든다. -->
+  <!-- 조건문 작성 v-show vs v-if  -->
+  <!-- 공식문서 기준 v-if는 toggle시 비용이 많이 발생하고 v-show는 초기 렌더링에 비용이 많이든다. -->
   <div v-show="toggle">v-show = true</div>
   <div v-show="!toggle">v-show = false</div>
   <div v-if="toggle">v-if = true</div>
   <div v-else>v-else = false</div>
-  <div></div>
   <button @click="onToggle">Toggle</button>
+  <!-- container -->
   <div class="container">
     <h2>To-Do List</h2>
     <!-- @submit.prevent = event.preventDefault -->
-    <form @submit.prevent="onSubmit" class="d-flex">
-      <div class="flex-grow-1 mr-2">
-        <input
-          class="form-control"
-          type="text"
-          v-model="todo"
-          placeholder="Type new to-do"
-        />
+    <form @submit.prevent="onSubmit">
+      <div class="d-flex">
+        <div class="flex-grow-1 mr-2">
+          <input
+            class="form-control"
+            type="text"
+            v-model="todo"
+            placeholder="Type new to-do"
+          />
+        </div>
+        <div>
+          <button type="submit" class="btn btn-primary">Add</button>
+        </div>
       </div>
-      <div>
-        <button type="submit" class="btn btn-primary">Add</button>
+      <div v-show="hasError" style="color: red">
+        빈 내용은 등록이 불가합니다.
       </div>
     </form>
     <div v-for="todoItem in todos" :key="todoItem.id" class="card mt-2">
@@ -35,27 +40,37 @@ import {ref} from "vue";
 
 export default {
   setup() {
+    // field
     const toggle = ref(false);
     const todo = ref("");
     const todos = ref([
       {id: 1, subject: "휴대폰 사기"},
-
       {id: 2, subject: "장보기"},
     ]);
+    const hasError = ref(false);
+
+    // method
     function onToggle() {
       toggle.value = !toggle.value;
     }
+
     function onSubmit() {
-      todos.value.push({
-        id: Date.now(),
-        subject: todo.value,
-      });
+      if (todo.value.length <= 0) {
+        hasError.value = true;
+      } else {
+        hasError.value = false;
+        todos.value.push({
+          id: Date.now(),
+          subject: todo.value,
+        });
+      }
     }
 
     return {
       todo,
       todos,
       toggle,
+      hasError,
       onToggle,
       onSubmit,
     };
