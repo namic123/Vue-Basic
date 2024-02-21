1393be7fffd2cb8524e894df7cf19423020a3466
jay sung <namic123@naver.com>
2024-02-21 19:05:41 +0900

    (Html에서 조건문 작성) v-if vs v-show : 공식문서 기준 v-if는 toggle시 비용이 많이 발생하고 v-show는 초기 렌더링에 비용이 많이든다.

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,12 @@
 <template>
+<!-- 조건문 작성 v-show vs v-if  -->
+<!-- 공식문서 기준 v-if는 toggle시 비용이 많이 발생하고 v-show는 초기 렌더링에 비용이 많이든다. -->
+  <div v-show="toggle">v-show = true</div>
+  <div v-show="!toggle">v-show = false</div>
+  <div v-if="toggle">v-if = true</div>
+  <div v-else>v-else = false</div>
+  <div></div>
+  <button @click="onToggle">Toggle</button>
   <div class="container">
     <h2>To-Do List</h2>
     <!-- @submit.prevent = event.preventDefault -->
@@ -27,12 +35,16 @@ import {ref} from "vue";
 
 export default {
   setup() {
+    const toggle = ref(false);
     const todo = ref("");
     const todos = ref([
       {id: 1, subject: "휴대폰 사기"},
 
       {id: 2, subject: "장보기"},
     ]);
+    function onToggle() {
+      toggle.value = !toggle.value;
+    }
     function onSubmit() {
       todos.value.push({
         id: Date.now(),
@@ -43,6 +55,8 @@ export default {
     return {
       todo,
       todos,
+      toggle,
+      onToggle,
       onSubmit,
     };
   },
