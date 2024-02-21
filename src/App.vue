eaa5ed8ba93ba55d4d9d7f13ac5da5fca64ac7d5
jay sung <namic123@naver.com>
2024-02-21 12:58:14 +0900

    양방향 바인딩 원리 에시 코드

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,7 @@
-<!--Vue 컴포넌트는 크게 3가지 요소로 구성 -->
-<!--1. template이 있고, 이 안에는 html 코드가 구성-->
-<!--2. script, js 코드가 들어간다. -->
-<!--3. style, css 코드가 들어간다 -->
 <template>
-  <!-- 아래 script 코드의 변수 및 함수 호출 -->
   <div>
-    <!-- 데이터 바인딩(데이터 간 연결) 예시 -->
-    <!-- 모든 태그의 속성(attribute)에 데이터 바인딩이 가능(선택자 속성도 포함) -->
-    <!-- v-bind는 생략이 가능하다 v-bind: = : -->
-    <div :class="changeClass">{{ name }}</div>
-    <input :type="type" :value="name" />
-    <!-- v-on:click 도 @click으로 생략 가능 -->
-    <button 
-    class="btn btn-primary" 
-    @click="updateName"
-    >Click</button>
+    <input :type="type" :value="name" @input="updateName" />
+    <button class="btn btn-primary" @click="onSubmit">Click</button>
   </div>
 </template>
 <script>
@@ -24,18 +11,15 @@ export default {
   setup() {
     // ref
     let name = ref("Jay park(ref)");
-    let type = ref("text");
-    let changeClass = ref("name");
-
-    function updateName() {
-      name.value = "changed to jay by ref!";
-      type.value = "number";
-      changeClass.value = "changeName";
+    function onSubmit() {
+      console.log(name.value);
+    }
+    function updateName(event) {
+      name.value = event.target.value;
     }
     return {
       name,
-      type,
-      changeClass,
+      onSubmit,
       updateName,
     };
   },
