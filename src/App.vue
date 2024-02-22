2091bea3015ae5e5a373bdd421112cbb297e4e23
jay sung <namic123@naver.com>
2024-02-22 14:13:40 +0900

    필요없는 코드 삭제

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <!-- container -->
   <div class="container">
-    <h4>count: {{ count }}</h4>
-    <h4>double count:{{ doubleCountComputed }}</h4>
-    <h4>double count:{{ doubleCountMethod() }}</h4>
-    <button @click="count++">Add One</button>
     <h2>To-Do List</h2>
     <!-- 자식 컴포넌트에서 데이터를 보내고 addTodo 메서드 실행-->
     <TodoSimpleForm @add-todo="addTodo" />
@@ -55,20 +51,7 @@ export default {
       todos.value.push(todo);
     }
 
-    const count = ref(1);
 
-    // computed
-    const doubleCountComputed = computed(() => {
-      return count.value * 2;
-    });
-    // 일반 method
-    const doubleCountMethod = () => {
-      return count.value * 2;
-    };
-
-    // computed와 일반 method의 차이는 캐싱과 반응성에 차이가 있음.
-    // 메서드는 호출될 때마다 계산을 수행하지만, computed 속성은 의존하는 데이터가 '변경되었을 때만 계산을 수행'하고 그 결과를 캐싱함.
-    // 또한, computed는 자동으로 반응형 시스템에 등록되어 의존성을 추적한다.
 
     return {
       todos,
@@ -76,9 +59,6 @@ export default {
       deleteTodo,
       addTodo,
       handleComplete,
-      count,
-      doubleCountComputed,
-      doubleCountMethod,
     };
   },
 };
