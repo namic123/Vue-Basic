e4cb9f7a9680e36b03c26fa5c01167414016a16c
jay sung <namic123@naver.com>
2024-02-28 01:26:50 +0900

    router-link로 이동 경로 선언시 라우터의 고유 이름으로 지정이 가능하다.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <router-link class="navbar-brand" to="/">JaySung</router-link>
+    <router-link class="navbar-brand" :to="{name:'Home'}">JaySung</router-link>
 
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-          <router-link class="nav-link" to="/todos">Todos</router-link>
+          <router-link class="nav-link" :to="{name:'Todos'}">Todos</router-link>
         </li>
       </ul>
   </nav>
