67390c3d4cd94e331d48fd489e1f6500e83630de
jay sung <namic123@naver.com>
2024-02-27 16:18:03 +0900

    BootStrap nav바 사용

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,14 @@
 <template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">JaySung</a>
+
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Todos <span class="sr-only"></span></a>
+        </li>
+      </ul>
+  </nav>
+<!-- index.js에 정의한 url path 별 컴포넌트가 마운트됨. -->
   <router-view/>
 </template>
 <script>
