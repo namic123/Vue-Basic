b39b97b91e92ad815c5be73bd7dca3536dd360ca
jay sung <namic123@naver.com>
2024-03-02 01:10:44 +0900

    Vuex의 store, state, action, mutation을 활용하여 애플리케이션 전역에서 toast 기능 제공하기.

--- src/composables/toast.js
+++ src/composables/toast.js
@@ -1,28 +1,25 @@
-import {computed, onUnmounted} from 'vue';
+import {computed} from 'vue';
 import {useStore} from 'vuex';
 
 export function useToast(){
   const store = useStore();
   const showToast = computed(()=> store.state.showToast);
-  const toastMessage = computed(()=>store.state.toastMessage);
+  const toastMessage = computed(()=>store.getters.toastMessageWithSmile);
   const toastAlertType = computed(()=>store.state.toastAlertType);
   const timeout = computed(()=>store.state.timeout);
   function triggerToast(message, type='success'){
-    toastMessage.value = message;
-    toastAlertType.value = type;
-    showToast.value = true;
-    timeout.value = setTimeout(()=>{
-      toastMessage.value = "";
-      toastAlertType.value = "";
-      showToast.value = false;
-    },3000);
+    // toastMessage.value = message;
+    // toastAlertType.value = type;
+    // showToast.value = true;
+    // timeout.value = setTimeout(()=>{
+    //   toastMessage.value = "";
+    //   toastAlertType.value = "";
+    //   showToast.value = false;
+    // },3000);
+    store.dispatch('triggerToast', message, type);
   }
 
-  // 컴포넌트에 빠졌을 때 메모리를 정리하기 위해서 불필요한 triggerToast실행을 멈춤
-  onUnmounted(()=>{
-    // 해당 컴포넌트의 DOM에 빠진 후(예: 페이지 이동) 실행
-    clearTimeout(timeout.value); // setTimeout이 멈춤
-  })
+
   return{
     toastMessage,
     toastAlertType,
