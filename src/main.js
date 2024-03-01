7f1408cd443f82d9bb2730e08b24e762d4b79581
jay sung <namic123@naver.com>
2024-03-01 21:03:24 +0900

    vuex 설치 및 기본 설정 : Vue의 상태 관리 패턴 및 라이브러리.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
+import store from '@/store';
 
 // index.html에 id=app을 가진 div태그에 마운트
 // react #root와 유사
 // 즉, index.html #app id를 가진 div에 App.vue 컴포넌트가 마운트
-createApp(App).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
---
+++ src/store/index.js
@@ -0,0 +1,7 @@
+import {createStore} from 'vuex';
+
+export default createStore({
+  state:{
+  // Vue의 컴포넌트 staterk emfdjrka
+  },
+});
